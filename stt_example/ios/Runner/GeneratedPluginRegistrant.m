2bb57f05eec7315c8e810a138f1d1d18b873398c
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-17 20:53:19 +0900

    Add files via upload

---
+++ stt_example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<background_stt/BackgroundSttPlugin.h>)
+#import <background_stt/BackgroundSttPlugin.h>
+#else
+@import background_stt;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BackgroundSttPlugin registerWithRegistrar:[registry registrarForPlugin:@"BackgroundSttPlugin"]];
+}
+
+@end
